267dc227777c6d9e6369bf23dcb89c19527f36b9
Berkers, Geert G.J.W <geert.berkers@student.fontys.nl>
2017-01-18 01:08:12 +0100

    Finished Accu
    Can see results of accu now

--- app/src/main/java/geert/berkers/iphonereparatieasten/activitytest/AccuTestActivity.java
+++ app/src/main/java/geert/berkers/iphonereparatieasten/activitytest/AccuTestActivity.java
@@ -1,8 +1,12 @@
 package geert.berkers.iphonereparatieasten.activitytest;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -17,17 +21,27 @@ import geert.berkers.iphonereparatieasten.R;
 /**
  * Created by Geert.
  */
-@SuppressWarnings("FieldCanBeLocal")
 public class AccuTestActivity extends AppCompatActivity {
 
-    private TextView txtInfo;
-    private TextView txtQuestion;
+    private FrameLayout frame;
+
+    private TextView txtHealth;
+    private TextView txtPercent;
+    private TextView txtPlugged;
+    private TextView txtCharging;
+    private TextView txtTechnology;
+    private TextView txtTemp;
+    private TextView txtVoltage;
+
+    private ImageView resultHealth;
+    private ImageView resultPercent;
+    private ImageView resultPlugged;
+    private ImageView resultCharging;
+    private ImageView resultTechnology;
+    private ImageView resultTemp;
+    private ImageView resultVoltage;
 
     private boolean accuDetailsAreWorking;
-
-    private FrameLayout frame;
-    private FloatingActionButton fabWorking;
-    private FloatingActionButton fabNotWorking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +52,27 @@ public class AccuTestActivity extends AppCompatActivity {
         setTitle(getString(R.string.accu));
     }
 
+    @Override
+    protected void onResume() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+
+        registerReceiver(batteryInfoReceiver, intentFilter);
+        System.out.println("OnResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(batteryInfoReceiver);
+        super.onPause();
+    }
+
     private void initControls() {
-        txtInfo = (TextView) findViewById(R.id.txtInfo);
-        txtQuestion = (TextView) findViewById(R.id.txtQuestion);
+        TextView txtInfo = (TextView) findViewById(R.id.txtInfo);
+        TextView txtQuestion = (TextView) findViewById(R.id.txtQuestion);
 
         txtInfo.setText(R.string.info_test_accu);
         txtQuestion.setText(R.string.question_test_accu);
@@ -48,11 +80,9 @@ public class AccuTestActivity extends AppCompatActivity {
         frame = (FrameLayout) findViewById(R.id.frameLayout);
         frame.removeAllViews();
 
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.wifi_result_layout, frame, false);
-        frame.addView(view);
+        createInformationView();
 
-        fabNotWorking = (FloatingActionButton) findViewById(R.id.fabNotWorking);
+        FloatingActionButton fabNotWorking = (FloatingActionButton) findViewById(R.id.fabNotWorking);
         fabNotWorking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +90,7 @@ public class AccuTestActivity extends AppCompatActivity {
             }
         });
 
-        fabWorking = (FloatingActionButton) findViewById(R.id.fabWorking);
+        FloatingActionButton fabWorking = (FloatingActionButton) findViewById(R.id.fabWorking);
         fabWorking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,7 +99,29 @@ public class AccuTestActivity extends AppCompatActivity {
         });
 
         //TODO: Get battery information and update view
-        updateView();
+        //updateView();
+    }
+
+    private void createInformationView() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.accu_result_layout, frame, false);
+        frame.addView(view);
+
+        txtHealth = (TextView) findViewById(R.id.txtHealth);
+        txtPercent = (TextView) findViewById(R.id.txtPercentage);
+        txtPlugged = (TextView) findViewById(R.id.txtPlugged);
+        txtCharging = (TextView) findViewById(R.id.txtCharging);
+        txtTechnology = (TextView) findViewById(R.id.txtTechnology);
+        txtTemp = (TextView) findViewById(R.id.txtTemp);
+        txtVoltage = (TextView) findViewById(R.id.txtVoltage);
+
+        resultHealth = (ImageView) findViewById(R.id.resultHealth);
+        resultPercent = (ImageView) findViewById(R.id.resultPercentage);
+        resultPlugged = (ImageView) findViewById(R.id.resultPlugged);
+        resultCharging = (ImageView) findViewById(R.id.resultCharging);
+        resultTechnology = (ImageView) findViewById(R.id.resultTechnology);
+        resultTemp = (ImageView) findViewById(R.id.resultTemp);
+        resultVoltage = (ImageView) findViewById(R.id.resultVoltage);
     }
 
     private void isNotWorking() {
@@ -89,48 +141,118 @@ public class AccuTestActivity extends AppCompatActivity {
         finish();
     }
 
-    private void updateView() {
-
-        TextView txtNetwork = (TextView) findViewById(R.id.txtNetwork);
-        TextView txtBSSID = (TextView) findViewById(R.id.txtBSSID);
-        TextView txtIPAddress = (TextView) findViewById(R.id.txtIPAddress);
-
-        ImageView resultNetwork = (ImageView) findViewById(R.id.resultNetwork);
-        ImageView resultBSSID = (ImageView) findViewById(R.id.resultBSSID);
-        ImageView resultIPAddress = (ImageView) findViewById(R.id.resultIPAddress);
-
-        String bssid = "Test";
-        String ipAddress = "Test";
-        String network = "Test";
-
-        if (bssid != null && !bssid.equals("")) {
-            txtBSSID.setText(bssid);
-            resultBSSID.setImageResource(R.drawable.ic_check);
-            resultBSSID.setColorFilter(Color.GREEN);
-        } else {
-            resultBSSID.setImageResource(R.drawable.ic_uncheck);
-            resultBSSID.setColorFilter(Color.RED);
+    private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateBatteryData(intent);
         }
+    };
 
-        if (ipAddress != null && !ipAddress.equals("")) {
-            txtIPAddress.setText(ipAddress);
-            resultIPAddress.setImageResource(R.drawable.ic_check);
-            resultIPAddress.setColorFilter(Color.GREEN);
-        } else {
-            resultIPAddress.setImageResource(R.drawable.ic_uncheck);
-            resultIPAddress.setColorFilter(Color.RED);
+    @SuppressLint("SetTextI18n")
+    private void updateBatteryData(Intent intent) {
+        boolean present = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
+        if (present) {
+            String healthValue;
+            int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
+
+            switch (health) {
+                case BatteryManager.BATTERY_HEALTH_COLD:                healthValue = "Cold";                   break;
+                case BatteryManager.BATTERY_HEALTH_DEAD:                healthValue = "Dead";                   break;
+                case BatteryManager.BATTERY_HEALTH_GOOD:                healthValue = "Good";                   break;
+                case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:        healthValue = "Over Voltage";           break;
+                case BatteryManager.BATTERY_HEALTH_OVERHEAT:            healthValue = "Overheat";               break;
+                case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE: healthValue = "Unspecified failure";    break;
+                case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                default:                                                healthValue = "Unknown";                break;
+            }
+
+            if(healthValue.equals("Unknown")){
+                resultHealth.setImageResource(R.drawable.ic_uncheck);
+                resultHealth.setColorFilter(Color.RED);
+            }else {
+                txtHealth.setText(healthValue);
+                resultHealth.setImageResource(R.drawable.ic_check);
+                resultHealth.setColorFilter(Color.GREEN);
+            }
+
+            // Calculate Battery Percentage ...
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            if (level != -1 && scale != -1) {
+                int batteryPct = (int) ((level / (float) scale) * 100f);
+                txtPercent.setText(batteryPct + " %");
+                resultPercent.setImageResource(R.drawable.ic_check);
+                resultPercent.setColorFilter(Color.GREEN);
+            } else {
+                resultPercent.setImageResource(R.drawable.ic_uncheck);
+                resultPercent.setColorFilter(Color.RED);
+            }
+
+            int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+            String pluggedValue;
+
+            switch (plugged) {
+                case BatteryManager.BATTERY_PLUGGED_WIRELESS:   pluggedValue = "Wireless";  break;
+                case BatteryManager.BATTERY_PLUGGED_USB:        pluggedValue = "USB";       break;
+                case BatteryManager.BATTERY_PLUGGED_AC:         pluggedValue = "AC";        break;
+                default:                                        pluggedValue = "None";      break;
+            }
+
+            txtPlugged.setText(pluggedValue);
+            resultPlugged.setImageResource(R.drawable.ic_check);
+            resultPlugged.setColorFilter(Color.GREEN);
+
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            String statusValue;
+
+            switch (status) {
+                case BatteryManager.BATTERY_STATUS_CHARGING:        statusValue = "Opladen";    break;
+                case BatteryManager.BATTERY_STATUS_FULL:            statusValue = "Vol";        break;
+                case BatteryManager.BATTERY_STATUS_UNKNOWN:         statusValue = "Onbekend";   break;
+                case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                default:                                            statusValue = "Ontladen";   break;
+            }
+            txtCharging.setText(statusValue);
+            resultCharging.setImageResource(R.drawable.ic_check);
+            resultCharging.setColorFilter(Color.GREEN);
+
+            if (intent.getExtras() != null) {
+                String technology = intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+
+                if (!"".equals(technology)) {
+                    txtTechnology.setText(technology);
+                    resultTechnology.setImageResource(R.drawable.ic_check);
+                    resultTechnology.setColorFilter(Color.GREEN);
+                } else {
+                    resultTechnology.setImageResource(R.drawable.ic_uncheck);
+                    resultTechnology.setColorFilter(Color.RED);
+                }
+            }
+
+            int temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+            if (temperature > 0) {
+                float temp = ((float) temperature) / 10f;
+                txtTemp.setText(temp + "°C");
+                resultTemp.setImageResource(R.drawable.ic_check);
+                resultTemp.setColorFilter(Color.GREEN);
+            } else {
+                resultTemp.setImageResource(R.drawable.ic_uncheck);
+                resultTemp.setColorFilter(Color.RED);
+            }
+
+            int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+
+            if (voltage > 0) {
+                txtVoltage.setText(voltage + " mV");
+                resultVoltage.setImageResource(R.drawable.ic_check);
+                resultVoltage.setColorFilter(Color.GREEN);
+            } else {
+                resultVoltage.setImageResource(R.drawable.ic_uncheck);
+                resultVoltage.setColorFilter(Color.RED);
+            }
         }
-
-        if (network != null && !network.equals("")) {
-            txtNetwork.setText(network);
-            resultNetwork.setImageResource(R.drawable.ic_check);
-            resultNetwork.setColorFilter(Color.GREEN);
-        } else {
-            resultNetwork.setImageResource(R.drawable.ic_uncheck);
-            resultNetwork.setColorFilter(Color.RED);
-        }
-
-
     }
 }
 
